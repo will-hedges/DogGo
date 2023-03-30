da077f745e0016945392b0adb44e87ae0d1baa72
Will Hedges <wshedges@outlook.com>
2023-03-30 11:06:45 -0500

    Owner edit now takes view model, neighborhood selection

--- DogGo/Controllers/OwnersController.cs
+++ DogGo/Controllers/OwnersController.cs
@@ -92,7 +92,15 @@ namespace DogGo.Controllers
             {
                 return NotFound();
             }
-            return View(owner);
+            
+            List<Neighborhood> neighborhoods = _neighborhoodRepo.GetAll();
+
+            OwnerFormViewModel vm = new OwnerFormViewModel()
+            {
+                Owner = owner,
+                Neighborhoods = neighborhoods
+            };
+            return View(vm);
         }
 
         // POST: Owners/Edit/5
