d38b1aadd5a0dc71167b8cd3bdc7e822378d05ea
Will Hedges <wshedges@outlook.com>
2023-04-02 22:51:05 -0500

    users not logged in will see a list of all walkers at /walkers

--- DogGo/Controllers/WalkersController.cs
+++ DogGo/Controllers/WalkersController.cs
@@ -3,26 +3,49 @@ using DogGo.Models.ViewModels;
 using DogGo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace DogGo.Controllers
 {
     public class WalkersController : Controller
     {
         private readonly IWalkerRepository _walkerRepo;
+        private readonly IOwnerRepository _ownerRepo;
 
         // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
-        public WalkersController(IWalkerRepository walkerRepository)
+        public WalkersController(IWalkerRepository walkerRepository, IOwnerRepository ownerRepo)
         {
             _walkerRepo = walkerRepository;
+            _ownerRepo = ownerRepo;
+        }
+
+        private int GetCurrentUserId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
         }
 
         // GET: Walkers
         public ActionResult Index()
         {
-            List<Walker> walkers = _walkerRepo.GetAllWalkers();
-
-            return View(walkers);
+            // if a user is logged in, only show walkers in their neighborhood
+            try
+            {
+                // get the user's Id
+                int currentOwnerId = GetCurrentUserId();
+                // get the owner object 
+                Owner currentOwner = _ownerRepo.GetOwnerById(currentOwnerId);
+                List<Walker> walkers = _walkerRepo.GetWalkersInNeighborhood(currentOwner.NeighborhoodId);
+                return View(walkers);
+            }
+            // else, show a list of all the walkers
+            catch (ArgumentNullException)
+            {
+                List<Walker> walkers = _walkerRepo.GetAllWalkers();
+                return View(walkers);
+            }
         }
 
         // GET: WalkersController/Details/5
